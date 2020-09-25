bcec56e6c8e3051124491753fe2d2cbd367aa1dd
AdrKacz <61620013+AdrKacz@users.noreply.github.com>
2020-09-25 19:36:56 +0200

    Forgot Helmet Activation
    Was Commented for Development

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// app.use(helmet()); //add headers that help protect the app
+app.use(helmet()); //add headers that help protect the app
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
